939934d607d901c0da1e7f45f203483c198e47df
Colin Carroll <colcarroll@gmail.com>
2019-01-08 18:04:49 -0500

    Documentation

--- strava_calendar/strava_calendar.py
+++ strava_calendar/strava_calendar.py
@@ -17,6 +17,41 @@ def plot_calendar(
     sport="running",
     label=None,
 ):
+    """Plot a year of Strava data in a calendar layout.
+
+    Parameters
+    ----------
+    zip_path : str
+        Path to .zip archive from Strava
+    year : int
+        Year of data to use. We have to unzip and read each file in the archive
+        to figure out what year it is from, and this takes around 5 minutes for
+        a new year of data.
+    plot_size : float (default=1)
+        The size of the plot is dynamically chosen for the layout, but you can make
+        it bigger or smaller by making this number bigger or smaller.
+    n_cols : int (default=4)
+        Number of columns to divide the days into. Splits evenly on months, so
+        this number should evenly divide 12.
+    month_gap : float (default=0)
+        Vertical space between two months. Each calendar square is 1 x 1, so
+        a value of 1.5 here would move the months 1.5 calendar squares apart.
+    col_gap : float (default=0.5)
+        Horizontal space between columns. A calendar square is 1 x 1, so a
+        value of 0.5 here puts columns half a square apart.
+    sport : str (default="running")
+        Sport to plot routes for. I have not tested this with anything except
+        running, but maybe you get lucky!
+    label : str or None
+        Label in the top left corner of the plots. Defaults to the year. Use ""
+        to not have any label.
+
+    Returns
+    -------
+    figure, axis
+        The matplotlib figure and axis with the plot. These can be used
+        for further customization.
+    """
     data = get_data(
         zip_path,
         sport,
